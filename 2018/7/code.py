2f42eb9c5a1779f3c805b1ff9b2028d7f980be7b
Mathieu AGUESSE <maguesse@gmail.com>
2018-12-19 23:03:11 +0100

    Day 7 - Part 2

--- 2018/7/code.py
+++ 2018/7/code.py
@@ -4,7 +4,7 @@ from itertools import groupby
 from collections import defaultdict, deque
 
 
-def process(instructions):
+def process_part1(instructions):
     result = list()
     steps = set([i[0] for i in instructions] +
                 [i[1] for i in instructions])
@@ -26,10 +26,50 @@ def process(instructions):
                 roots.append(dest)
         roots = deque(sorted(roots))
 
+    return ''.join(result)
 
-    p1 = ''.join(result)
-    p2 = None
-    return p1, p2
+
+def end_time(step, start_time, offset):
+    return start_time + offset + 1 + ord(step) - ord('A')
+
+
+def do_work(start_time, nb_workers, offset,
+            available_tasks, working_queue):
+    while len(working_queue) < nb_workers and available_tasks:
+        current_task = available_tasks.popleft()
+        working_queue.append((end_time(current_task, start_time, offset)
+                              , current_task))
+    return available_tasks, deque(sorted(working_queue))
+
+
+def plan_work(nb_workers, offset, edges, in_depth):
+    available_tasks, working_queue = do_work(0, nb_workers, offset,
+                                             deque([t for t in edges if
+                                                    in_depth[t] == 0]),
+                                             deque([]))
+    while working_queue or available_tasks:
+        time, task = working_queue.popleft()
+        for dest in edges[task]:
+            in_depth[dest] -= 1
+            if in_depth[dest] == 0:
+                available_tasks.append(dest)
+        available_tasks, working_queue = do_work(time, nb_workers, offset,
+                                                 available_tasks,
+                                                 working_queue)
+
+    return time
+
+
+
+def process_part2(instructions, nb_workers=5, offset=60):
+    edges = defaultdict(list)
+    in_depth = defaultdict(int)
+    for src, dest in instructions:
+        edges[src].append(dest)
+        in_depth[dest] += 1
+    for src in edges:
+        edges[src] = sorted(edges[src])
+    return plan_work(nb_workers, offset, edges, in_depth)
 
 def load_input(filename):
     ptn = re.compile(' ([A-Z]) ')
@@ -37,6 +77,9 @@ def load_input(filename):
                 for line in open(filename, 'r'))
 
 if __name__=='__main__':
-    assert ('CABDFE', None) == process(load_input('test.txt'))
+    assert ('CABDFE', 15) == (process_part1(load_input('test.txt')),
+                                process_part2(load_input('test.txt'),
+                                              nb_workers=2, offset=0))
     print('\n' + '*'*80 +'\n')
-    print(process(load_input('input')))
+    print(process_part1(load_input('input')),
+          process_part2(load_input('input')))
