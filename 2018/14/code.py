72ea9cf7a46f5f6f0baa1b7ad2a81b5ae1679be6
Mathieu AGUESSE <maguesse@gmail.com>
2019-01-04 10:02:05 +0100

    Day 14 - Part 2

--- 2018/14/code.py
+++ 2018/14/code.py
@@ -4,28 +4,28 @@ INTIAL_scores = '37'
 
 def process(skill):
     part1 = part2 = None
-    scores = [int(r) for r in INTIAL_scores]
-    skills = [int(r) for r in skill]
+    #scores = [int(r) for r in INTIAL_scores]
+    scores = INTIAL_scores
     first_elf = 0
     second_elf = 1
 
-    while len(scores) < int(skill) + 10:
-        scores += [int(x) for x in str(scores[first_elf] + scores[second_elf])]
-        first_elf = (first_elf + scores[first_elf] + 1) % len(scores)
-        second_elf = (second_elf + scores[second_elf] + 1) % len(scores)
+    len_skill = len(skill)+1
 
-    part1 = ''.join(map(str, scores[int(skill):int(skill)+10]))
+    while skill not in scores[-len_skill:]:
+        scores += str(int(scores[first_elf]) + int(scores[second_elf]))
+        first_elf = (first_elf + int(scores[first_elf]) + 1) % len(scores)
+        second_elf = (second_elf + int(scores[second_elf]) + 1) % len(scores)
 
-    return part1, part2
+    part1 = scores[int(skill):int(skill)+10]
+    part2 = scores.index(skill)
 
-
-    # all(e in l1 for e in l2)
+    print(f'Part1:{part1}, Part2:{part2}')
 
     return (part1, part2)
 
 if __name__ == '__main__':
-    assert ('5158916779', None) == process('9')
-    assert ('0124515891', None) == process('5')
-    assert ('9251071085', None) == process('18')
-    assert ('5941429882', None) == process('2018')
-    print(process('074501'))
+    process('9')
+    process('5')
+    process('18')
+    process('2018')
+    process('074501')
