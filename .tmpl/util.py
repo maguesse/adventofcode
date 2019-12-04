94967ce0a0b0010295b229d63f049cf0964bdf23
Mathieu AGUESSE <maguesse@gmail.com>
2019-12-04 23:29:11 +0100

    Add new load function

--- .tmpl/util.py
+++ .tmpl/util.py
@@ -5,3 +5,14 @@ def load_data(formater=None):
     if formater is not None:
         res = [formater(line) for line in res]
     return res
+
+def load_list_of_ints():
+    res = []
+    with open('input') as input:
+        res = [int(i) for line in
+               input.read().splitlines() for i in line.split(',')]
+    return res
+
+def load_lists_of_strings():
+    data = [line.rstrip('\r\n').split(',') for line in open('input')]
+    return data
