dff8fe9b440deaa22691cc897001a63788779384
Mathieu AGUESSE <maguesse@gmail.com>
2019-12-23 00:20:30 +0100

    Day 7 - Part 2

--- 2019/7/intcode.py
+++ 2019/7/intcode.py
@@ -16,22 +16,24 @@ class AddressingMode(IntEnum):
 def debug_fun(fn):
     @functools.wraps(fn)
     def wrapper(*args, **kargs):
-        if DEBUG:
-            print(f'DEBUG fn:{fn.__name__}, args:{args}, kargs={kargs}')
-            return fn(*args, **kargs)
+        #if DEBUG:
+        #    print(f'DEBUG fn:{fn.__name__}, args:{args}, kargs={kargs}')
+        #    return fn(*args, **kargs)
         return fn(*args, **kargs)
 
     return wrapper
 
 class IntCode:
 
-    def __init__(self, prg):
+    def __init__(self, prg, cpu_id=-1):
         self.pc = 0
         self.input = deque()
         self.output = deque()
         self.last_output = -1
         self.is_running = True
         self.is_waiting = False
+        self.is_halted = False
+        self.cpu_id = cpu_id
         self.prg = prg.copy()
         self.instructions = {
             1:(self.op_add, AccesMode.READ, AccesMode.READ, AccesMode.WRITE),
@@ -51,6 +53,16 @@ class IntCode:
         self.flags = {
             'jmp':False
         }
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} #{self.cpu_id} with input {list(self.input)}'\
+            f' and output {list(self.output)}' \
+            f' is {"not " if not self.is_running else ""}running' \
+            f', is {"not " if not self.is_halted else ""}halted'\
+            f', is {"not " if not self.is_waiting else ""}waiting' \
+            f', last output was {self.last_output}' \
+            f', pc is {self.pc}'\
+            f', prg is {self.prg}>'
 
     @staticmethod
     @debug_fun
@@ -74,8 +86,6 @@ class IntCode:
 
     @debug_fun
     def fn_fetch_value(self, mode):
-        #if mode[0] == AccesMode.WRITE:
-        #    return self.value_fetchers[AddressingMode.POSITION]
         return self.value_fetchers[mode[0] + mode[1]]
 
     @debug_fun
@@ -109,6 +119,10 @@ class IntCode:
     @debug_fun
     def op_input(self, operands):
         addr = operands.pop()
+        if len(self.input) == 0:
+            self.is_waiting = True
+            self.inc(-1)
+            return
         self.prg[addr] = self.input.popleft()
         self.inc()
 
@@ -118,6 +132,7 @@ class IntCode:
         self.output.append(v)
         self.last_output = self.output[-1]
         self.inc()
+        self.is_waiting = True
 
     @debug_fun
     def op_jmp_not_zero(self, operands):
@@ -150,66 +165,121 @@ class IntCode:
     @debug_fun
     def op_halt(self, _):
         self.is_running = False
+        self.is_halted = True
 
     def run(self, input):
         self.input = deque(input)
-        while self.is_running:
+        if self.is_halted:
+            print("IntCode computer is halted.")
+            return
+        self.is_waiting = False
+        if DEBUG: print(f'Running {self}')
+        while self.is_running and not self.is_waiting:
             self.flags['jmp'] = False
             # Get OpCode and addressing modes
             opcode, *addressing_modes=IntCode.decode_opcode(self.prg[self.pc])
             # Get operation and its operands access modes
             op, *opmodes = self.instructions[opcode]
-            if DEBUG: print(f'DEBUG pc:{self.pc}, prg:{self.prg}')
-            if DEBUG: print(f'DEBUG opcode: {opcode}, op:{op.__name__}')
-            if DEBUG: print(f'DEBUG opcode:{opmodes}, addressing_modes:{addressing_modes}')
+            if DEBUG: print(80*'*')
+            if DEBUG: print(f'DEBUG CPU #{self.cpu_id} pc:{self.pc}, prg:{self.prg}')
+            if DEBUG: print(f'DEBUG CPU #{self.cpu_id} opcode: {opcode}, op:{op.__name__}')
+            if DEBUG: print(f'DEBUG CPU #{self.cpu_id} opcode:{opmodes}, addressing_modes:{addressing_modes}')
             # Associate each operand acces mode with its addressing mode
             modes = list(zip_longest(opmodes, addressing_modes,
                                      fillvalue=AddressingMode.POSITION))
-            if DEBUG: print(f'DEBUG mode:{modes}')
+            if DEBUG: print(f'DEBUG CPU #{self.cpu_id} mode:{modes}')
             op(self.fetch_values(modes))
+            if DEBUG: print(f'State {self}')
         return self.last_output
 
 
 if __name__ == '__main__':
-     # Day 2
+    # Day 2
+    test='Day 2 - Test 1'
     IntCode([1,9,10,3,2,3,11,0,99,30,40,50]).run([])
+    print(f'{test} OK')
+    test='Day 2 - Test 2'
     IntCode([1,0,0,0,99]).run([])
+    print(f'{test} OK')
+    test='Day 2 - Test 3'
     IntCode([2,3,0,3,99]).run([])
+    print(f'{test} OK')
+    test='Day 2 - Test 4'
     IntCode([2,4,4,5,99,0]).run([])
+    print(f'{test} OK')
+    test='Day 2 - Test 5'
     IntCode([1,1,1,4,99,5,6,0,99]).run([])
+    print(f'{test} OK')
 
     # Day 5 tests
     # Equals 8 ?
-    assert 1 == IntCode([3,9,8,9,10,9,4,9,99,-1,8]).run([8])
-    assert 0 == IntCode([3,9,8,9,10,9,4,9,99,-1,8]).run([5])
+    test='Day 5 - Test 1.1 - Input equals 8? (Position mode)'
+    assert 1 == IntCode([3,9,8,9,10,9,4,9,99,-1,8]).run([8]), f'{test} KO'
+    print(f'{test} OK')
+    test='Day 5 - Test 1.2 - Input equals 8? (Position mode)'
+    assert 0 == IntCode([3,9,8,9,10,9,4,9,99,-1,8]).run([5]), f'{test} KO'
+    print(f'{test} OK')
 
     # Less than 8 ?
-    assert 0 == IntCode([3,9,7,9,10,9,4,9,99,-1,8]).run([8])
-    assert 1 == IntCode([3,9,7,9,10,9,4,9,99,-1,8]).run([5])
+    test='Day 5 - Test 2.1 - Input less than 8? (Position mode)'
+    assert 0 == IntCode([3,9,7,9,10,9,4,9,99,-1,8]).run([8]), f'{test} KO'
+    print(f'{test} OK')
+    test='Day 5 - Test 2.2 - Input less than 8? (Position mode)'
+    assert 1 == IntCode([3,9,7,9,10,9,4,9,99,-1,8]).run([5]), f'{test} KO'
+    print(f'{test} OK')
 
     # Equals 8 ?
-    assert 1 == IntCode([3,3,1108,-1,8,3,4,3,99]).run([8])
-    assert 0 == IntCode([3,3,1108,-1,8,3,4,3,99]).run([5])
+    test='Day 5 - Test 3.1 - Input equals 8? (Immediate mode)'
+    assert 1 == IntCode([3,3,1108,-1,8,3,4,3,99]).run([8]), f'{test} KO'
+    print(f'{test} OK')
+    test='Day 5 - Test 3.2 - Input equals 8? (Immediate mode)'
+    assert 0 == IntCode([3,3,1108,-1,8,3,4,3,99]).run([5]), f'{test} KO'
+    print(f'{test} OK')
 
     # Less than 8 ?
-    assert 0 == IntCode([3,3,1107,-1,8,3,4,3,99]).run([8])
-    assert 1 == IntCode([3,3,1107,-1,8,3,4,3,99]).run([5])
+    test='Day 5 - Test 4.1 - Input less than 8? (Immediate mode)'
+    assert 0 == IntCode([3,3,1107,-1,8,3,4,3,99]).run([8]), f'{test} KO'
+    print(f'{test} OK')
+    test='Day 5 - Test 4.2 - Input less than 8? (Immediate mode)'
+    assert 1 == IntCode([3,3,1107,-1,8,3,4,3,99]).run([5]), f'{test} KO'
+    print(f'{test} OK')
 
     # Zero ?
-    assert 0 == IntCode([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]).run([0])
-    assert 1 == IntCode([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]).run([1])
+    test='Day 5 - Test 5.1 - Input is 0? (Position mode)'
+    assert 0 == IntCode([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]).run([0]), f'{test} KO'
+    print(f'{test} OK')
+    test='Day 5 - Test 5.2 - Input is 0? (Position mode)'
+    assert 1 == IntCode([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]).run([1]), f'{test} KO'
+    print(f'{test} OK')
 
     # Zero ?
-    assert 0 == IntCode([3,3,1105,-1,9,1101,0,0,12,4,12,99,1]).run([0])
-    assert 1 == IntCode([3,3,1105,-1,9,1101,0,0,12,4,12,99,1]).run([1])
+    test='Day 5 - Test 6.1 - Input is 0? (Immediate mode)'
+    assert 0 == IntCode([3,3,1105,-1,9,1101,0,0,12,4,12,99,1]).run([0]), f'{test} KO'
+    print(f'{test} OK')
+    test='Day 5 - Test 6.2 - Input is 0? (Immediate mode)'
+    assert 1 == IntCode([3,3,1105,-1,9,1101,0,0,12,4,12,99,1]).run([1]), f'{test} KO'
+    print(f'{test} OK')
 
     # Compare to 8
+    test='Day 5 - Test 7.1 - Compare to 8'
     assert 999 == IntCode([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
              1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
-             999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]).run([7])
+             999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]).run([7]), f'{test} KO'
+    print(f'{test} OK')
+    test='Day 5 - Test 7.2 - Compare to 8'
     assert 1000 == IntCode([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
              1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
-             999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]).run([8])
+             999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]).run([8]), f'{test} KO'
+    print(f'{test} OK')
+    test='Day 5 - Test 7.3 - Compare to 8'
     assert 1001 == IntCode([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
              1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
-             999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]).run([9])
+             999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]).run([9]), f'{test} KO'
+    print(f'{test} OK')
+
+    # Amplifier
+    test='Day 7 - Test 1.1 - Amplifier Feedback'
+    assert 5 == IntCode([3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+                         27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]).run([9, 0]), f'{test} KO'
+    assert 7 == IntCode([3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+                         27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]).run([9, 1]), f'{test} KO'
--- 2019/7/main.py
+++ 2019/7/main.py
@@ -6,48 +6,101 @@ from itertools import permutations
 NB_PHASES = 4
 NB_AMPS = 5
 
-def amp(index, first, second, prg):
-    return intcode.IntCode(prg).run([first, second])
+class Amplifier:
+    def __init__(self, prg, phase, id=-1):
+        from string import ascii_uppercase
+        self._input_signal = [phase]
+        self._output_signal = 0
+        self._id = ascii_uppercase[id]
+        self.phase = phase
+        self.ic = intcode.IntCode(prg, self.id)
+        self.done = False
 
-def eval_ampsignal(prg, settings):
-    instructions = settings[::-1]
-    signal = 0
-    for a in range(NB_AMPS):
-        signal = amp(a, instructions.pop(), signal, prg)
-    return signal
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def output_signal(self):
+        return self._output_signal
+
+    @output_signal.setter
+    def output_signal(self, output_signal):
+        self._output_signal = output_signal
+
+    @property
+    def input_signal(self):
+        return self._input_signal
+
+    @input_signal.setter
+    def input_signal(self, input_signal):
+        self._input_signal.append(input_signal)
 
 
-def part1(prg):
-    sequences = map(list, permutations(range(5)))
+    def run(self):
+        if not self.ic.is_halted:
+            # Popall input before calling IC
+            signal, self._input_signal = self._input_signal, []
+            self.ic.run(signal)
+            self.output_signal = self.ic.last_output
+        else:
+            self.done = True
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} #{self.id} on phase {self.phase}' \
+            f', input_signal={self._input_signal}' \
+            f', output_signal={self._output_signal}, done={self.done}>'
+
+
+    def __str__(self):
+        return f'<{self.__class__.__name__} #{self.id} on phase {self.phase}' \
+            f', input_signal={self._input_signal}' \
+            f', output_signal={self._output_signal}, done={self.done}>'
+
+def eval_ampsignal(prg, settings, feedback=False):
+    amps = []
+    for i, phase in enumerate(settings):
+        amps.append(Amplifier(prg, phase, id=i))
+
+    if feedback == False:
+        prev_signal = 0
+        for amp in amps:
+            amp.input_signal = prev_signal
+            amp.run()
+            prev_signal = amp.output_signal
+    else:
+        running = True
+        prev_signal = 0
+        while running:
+            for amp in amps:
+                amp.input_signal = prev_signal
+                amp.run()
+                prev_signal = amp.output_signal
+                if amp.done:
+                    # Stop when at least one amplifier as halted
+                    running = False
+    return prev_signal
+
+
+def solve_part1(prg):
+    sequences = map(list, permutations(range(NB_AMPS)))
     max_signal = max(((s, eval_ampsignal(prg.copy(), s)) for s in sequences),
                        key=lambda x: x[1])
-    return max_signal[1]
+    return max_signal
 
-def part2(prg):
-    sequences = map(list, permutations(range(5,10)))
-    # TODO: implement a solution
-    return 0
+def solve_part2(prg):
+    sequences = map(list, permutations(range(NB_AMPS,NB_AMPS*2)))
+    max_signal = max(((s, eval_ampsignal(prg.copy(), s, feedback=True))
+                      for s in sequences),
+                       key=lambda x: x[1])
+    return max_signal
 
 def solve(part2=False):
     data = util.load_list_of_ints()
     if part2 is False:
-        print(f'Part1: {part1(data)}')
+        print(f'Part1: {solve_part1(data)}')
     else:
-        print(f'Part2: {part2(data)}')
-
-class Puzzle:
-
-    def __init__(self):
-        self.data = util.load_data()
-
-    def init_data(self, data):
-        self.data = data
-
-    def solve_part1(self):
-        pass
-
-    def solve_part2(self):
-        pass
+        print(f'Part2: {solve_part2(data)}')
 
 if __name__ == '__main__':
     ex1 = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
@@ -55,9 +108,9 @@ if __name__ == '__main__':
            101,5,23,23,1,24,23,23,4,23,99,0,0]
     ex3 = [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
            1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]
-    assert 43210 == part1(ex1)
-    assert 54321 == part1(ex2)
-    assert 65210 == part1(ex3)
+    assert ([4,3,2,1,0], 43210) == solve_part1(ex1)
+    assert ([0,1,2,3,4], 54321) == solve_part1(ex2)
+    assert ([1,0,4,3,2], 65210) == solve_part1(ex3)
     solve()
     ex4 = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,
            26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
@@ -65,7 +118,7 @@ if __name__ == '__main__':
            -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
            53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
 
-    assert 139629729 == part2(ex4)
-    assert 18216 == part2(ex5)
+    assert ([9,8,7,6,5], 139629729) == solve_part2(ex4)
+    assert ([9,7,8,5,6], 18216) == solve_part2(ex5)
     solve(part2=True)
 
